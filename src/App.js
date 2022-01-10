b43bcd4e32e9181f9389152423dcf90ed8548f42
karlaferry <karlacarreon.1994@gmail.com>
2022-01-10 17:27:30 +0000

    setup header and home placeholders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Home from "./components/route.Home/Home";
 function App() {
   return (
-    <div>Hello!</div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
