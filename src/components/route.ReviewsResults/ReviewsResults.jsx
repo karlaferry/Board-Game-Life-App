0a111e3373c0ecee30f7c3d1de45805894fc75aa
karlaferry <karlacarreon.1994@gmail.com>
2022-01-12 12:39:56 +0000

    extract reviewcard

--- src/components/route.ReviewsResults/ReviewsResults.jsx
+++ src/components/route.ReviewsResults/ReviewsResults.jsx
@@ -1,9 +1,13 @@
+// Dependency Imports
 import { React, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchReviews } from "../../utils/api";
 import { capitaliseString } from "../../utils/utilFuncs";
+
+// Component Imports
 import Header from "../Header";
 import SearchBox from "../SearchBox";
+import ReviewCard from "./ReviewCard";
 
 export default function ReviewsResults() {
   const [displayedReviews, setDisplayedReviews] = useState([]);
@@ -20,15 +24,7 @@ export default function ReviewsResults() {
       <Header />
       <SearchBox />
       <h2>{capitaliseString(category)}</h2>
-      {displayedReviews.length <= 0 ? (
-        <p>No results.</p>
-      ) : (
-        <ul>
-          {displayedReviews.map((review) => (
-            <li key={review.review_id}>{review.title}</li>
-          ))}
-        </ul>
-      )}
+      <ReviewCard displayedReviews={displayedReviews} />
     </div>
   );
 }
