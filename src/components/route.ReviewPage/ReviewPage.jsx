6403c3d1285b0640c8ff8dfa00421305d6f5dbe5
karlaferry <karlacarreon.1994@gmail.com>
2022-01-12 12:39:26 +0000

    create reviewpage

---
+++ src/components/route.ReviewPage/ReviewPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Header";
+import SearchBox from "../SearchBox";
+
+export default function ReviewPage() {
+  return (
+    <div>
+      <Header />
+      <SearchBox />
+    </div>
+  );
+}
