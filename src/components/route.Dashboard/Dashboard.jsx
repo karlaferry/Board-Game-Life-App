ceccddfa20f2d68c6c5eef5beadedf31b5ad0506
karlaferry <karlacarreon.1994@gmail.com>
2022-01-12 11:36:03 +0000

    create dashboard

--- src/components/route.Dashboard/Dashboard.jsx
+++ src/components/route.Dashboard/Dashboard.jsx
@@ -1,16 +1,17 @@
 import { React, useContext } from "react";
-import { useParams } from "react-router-dom";
-import Header from "../Header";
 import { UserContext } from "../../contexts/UserContext";
+import Header from "../Header";
 
 export default function Dashboard() {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
-  const { username } = useParams();
-  console.log(currentUser);
+  const {
+    currentUser: { username, avatar_url, name },
+    setCurrentUser,
+  } = useContext(UserContext);
+  const firstName = name.split(" ")[0];
   return (
     <div>
       <Header />
-      <h2>Hello, {currentUser.username}!</h2>
+      <h2>Hello, {firstName}!</h2>
       <p>This is the dashboard.</p>
     </div>
   );
