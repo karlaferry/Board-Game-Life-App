623764e35168e4086fc4945990d8d9e5d21f3ab0
karlaferry <karlacarreon.1994@gmail.com>
2022-01-12 13:55:49 +0000

    remove useParams

--- src/components/route.ReviewPage/ReviewPage.jsx
+++ src/components/route.ReviewPage/ReviewPage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useParams } from "react-router";
 import Header from "../Header";
 import SearchBox from "../SearchBox";
 
 export default function ReviewPage() {
+  const { review_id } = useParams();
   return (
     <div>
       <Header />
       <SearchBox />
+      <h2></h2>
     </div>
   );
 }
